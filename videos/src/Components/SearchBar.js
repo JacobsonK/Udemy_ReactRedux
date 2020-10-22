98d12f2a1b2cc3e8ed60289cfb5ea42702a167e3
Kyler Jacobson <kylerjacobson@protonmail.com>
2020-10-22 15:25:28 -0700

    Finshed Videos App / Created Widgets App

--- videos/src/Components/SearchBar.js
+++ videos/src/Components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component{
         this.setState({ term: event.target.value})
     };
     
-    onFormSubmit = (event) =>{
+    onFormSubmit = (event) => {
         event.preventDefault();
 
         this.props.onFormSubmit(this.state.term);
